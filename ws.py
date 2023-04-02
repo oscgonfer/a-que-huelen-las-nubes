47da667485c50b6dc3d1446c2eb37b52ed0c2649
Adrian <adrian.hall@yale.edu>
2023-04-01 22:29:51 -0400

    attempt to restart on serial disconnect, also take port as cmd line arg

--- ws.py
+++ ws.py
@@ -2,21 +2,37 @@ from lib.server import Server
 from lib.serial import Serial
 import asyncio
 import sys
+import time
 
-async def main():
-  server = Server(port=8000)
-  serial = Serial(
-    port = "/dev/cu.usbmodem142111101",
-    #port = "/dev/cu.usbmodem14201",
-    port="DEMO",
-    baudrate = 9600,
-    to_serial = server.to_serial,
-    from_serial = server.from_serial
-  )
-  
-  await asyncio.gather(
-    server.start(),
-    serial.start(),
-  )
 
-asyncio.run(main())
+async def main(server_port, serial_port):
+    server = Server(port=server_port)
+    serial = Serial(
+        port=serial_port,
+        baudrate=9600,
+        to_serial=server.to_serial,
+        from_serial=server.from_serial
+    )
+
+    await asyncio.gather(
+        server.start(),
+        serial.start(),
+    )
+
+if __name__ == "__main__":
+    server_port = 8000
+    if len(sys.argv) > 1:
+        serial_port = sys.argv[1]
+    else:
+        serial_port = "/dev/cu.usbmodem142111101"
+        #serial_port = "/dev/cu.usbmodem14201"
+        # serial_port="DEMO"
+
+    while True:
+        try:
+            asyncio.run(main(server_port, serial_port))
+        except OSError:
+            print("WARNING: Serial disconnected. Attempting to reconnect in 1 second...")
+            time.sleep(1)
+
+    # except Exception as e:
