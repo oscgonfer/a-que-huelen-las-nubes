c392541a71cd0f35a65e354b9dc2e59dc4f79e00
oscgonfer <oscgonfer@gmail.com>
2024-11-17 16:23:10 +0100

    Update hardware board code

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -4,12 +4,12 @@ void setup()
 {
     Serial.begin(115200);
     while (!Serial) { delay(1); } // wait until serial console is open, remove if not tethered to computer
-    Serial.println("Hello!");
 }
 
 
 void loop() {
-    float pot = analogRead(A0)/1024*255;
-    Serial.println(pot);
+    float pot = analogRead(A0);
+    float conv = pot/4095*255;
+    Serial.println(conv);
     delay(100);
 }
